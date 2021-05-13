2730baa54aa1fa0474f9db125c67bb7266f239e5
Marc Riera Casals <mrc2407@gmail.com>
2021-05-13 10:02:38 +0200

    Create workflows

---
+++ lib/dashy/schema.ex
@@ -0,0 +1,8 @@
+defmodule Dashy.Schema do
+  defmacro __using__(_) do
+    quote do
+      use Ecto.Schema
+      @timestamps_opts [type: :utc_datetime_usec]
+    end
+  end
+end
---
+++ priv/repo/migrations/20210513071547_create_workflows.exs
@@ -0,0 +1,17 @@
+defmodule Dashy.Repo.Migrations.CreateWorkflows do
+  use Ecto.Migration
+
+  def change do
+    create table(:workflows) do
+      add :external_id, :integer, null: false
+      add :name, :string, null: false
+      add :node_id, :string, null: false
+      add :path, :string, null: false
+      add :state, :string, null: false
+
+      timestamps(inserted_at: :created_at)
+    end
+
+    create unique_index(:workflows, [:external_id])
+  end
+end
