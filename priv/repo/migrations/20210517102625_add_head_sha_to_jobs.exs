31fdb65ac23210b791f9abae5cf7cdff5c1bd374
Marc Riera Casals <mrc2407@gmail.com>
2021-05-17 13:06:21 +0200

    Add head_sha to jobs

---
+++ priv/repo/migrations/20210517102625_add_head_sha_to_jobs.exs
@@ -0,0 +1,26 @@
+defmodule Dashy.Repo.Migrations.AddHeadShaToJobs do
+  use Ecto.Migration
+
+  def up do
+    alter table("workflow_run_jobs") do
+      add :head_sha, :string
+    end
+
+    execute """
+    UPDATE workflow_run_jobs SET head_sha = metadata->'head_sha'::text;
+    """
+    execute """
+    UPDATE workflow_run_jobs SET head_sha = REPLACE(head_sha, '"', '');
+    """
+
+    alter table(:workflow_run_jobs) do
+      modify :head_sha, :string, null: false
+    end
+  end
+
+  def down do
+    alter table(:workflow_run_jobs) do
+      remove :head_sha, :string, null: false
+    end
+  end
+end
