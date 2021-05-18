82ce6fc5865c0a97433e976b1310e0ae591c3877
Leonardo Diez <leiodd@gmail.com>
2021-05-18 16:31:22 +0200

    chore: add more dev seeds

--- priv/repo/dev_seeds.exs
+++ priv/repo/dev_seeds.exs
@@ -5,21 +5,30 @@ defmodule Dashy.DevSeeder do
 
   alias Dashy.Repositories
   alias Dashy.Repositories.Repository
+  alias Dashy.Workflows.Workflow
+  alias Dashy.WorkflowRuns.WorkflowRun
+  alias Dashy.WorkflowRunJobs.WorkflowRunJob
 
   alias Dashy.Repo
 
 
-  def create_repo do
-    {:ok, repo} = Repositories.create_repository(%{user: "decidim", name: "decidim-bulletin-board", branch: "develop"})
+  def create_repo(user, name, branch) do
+    {:ok, repo} = Repositories.create_repository(%{user: user, name: name, branch: branch})
     Dashy.Fetcher.update_workflows(repo)
     Dashy.Fetcher.update_workflow_runs(repo)
     Dashy.Fetcher.update_all_workflow_run_jobs(repo)
   end
+
   def delete_data do
+    Repo.delete_all(WorkflowRunJob)
+    Repo.delete_all(WorkflowRun)
+    Repo.delete_all(Workflow)
     Repo.delete_all(Repository)
   end
 
 end
 
 Dashy.DevSeeder.delete_data()
-Dashy.DevSeeder.create_repo()
+Dashy.DevSeeder.create_repo("decidim", "decidim-bulletin-board", "develop")
+Dashy.DevSeeder.create_repo("ether", "etherpad-lite", "develop")
+Dashy.DevSeeder.create_repo("discourse", "discourse", "master")
