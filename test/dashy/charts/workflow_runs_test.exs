26a7154e3fc4f1b997ad75a204a5e6c365aafad3
Leonardo Diez <leiodd@gmail.com>
2021-05-18 16:31:34 +0200

    test: fix broken tests

--- test/dashy/charts/workflow_runs_test.exs
+++ test/dashy/charts/workflow_runs_test.exs
@@ -4,6 +4,8 @@ defmodule Dashy.Charts.WorkflowRunsTest do
   describe "runs/1" do
     test "lists all runs" do
       sha = "1"
+      repo = insert(:repository)
+      workflow = insert(:workflow, repository: repo)
 
       insert(:workflow_run,
         head_sha: sha,
@@ -11,7 +13,8 @@ defmodule Dashy.Charts.WorkflowRunsTest do
         started_at: ~U[2021-05-16 09:00:00Z],
         completed_at: ~U[2021-05-16 10:00:00Z],
         status: "completed",
-        conclusion: "success"
+        conclusion: "success",
+        workflow: workflow
       )
 
       insert(:workflow_run,
@@ -20,21 +23,25 @@ defmodule Dashy.Charts.WorkflowRunsTest do
         started_at: ~U[2021-05-16 09:00:00Z],
         completed_at: ~U[2021-05-16 11:00:00Z],
         status: "completed",
-        conclusion: "success"
+        conclusion: "success",
+        workflow: workflow
       )
 
-      assert [%Dashy.Charts.Run{} = fetched_run] = Dashy.Charts.WorkflowRuns.runs()
+      assert [%Dashy.Charts.Run{} = fetched_run] = Dashy.Charts.WorkflowRuns.runs(repo)
 
       # 2 hours
       assert fetched_run.minutes == 120
       assert fetched_run.seconds == 7200
       assert fetched_run.time == ~U[2021-05-16 09:00:00Z]
       assert fetched_run.status == "success"
-      assert fetched_run.link == "https://github.com/decidim/decidim/commit/#{sha}"
+      assert fetched_run.link == "https://github.com/#{repo.user}/#{repo.name}/commit/#{sha}"
     end
 
     test "handles the state correctly" do
       sha = "1"
+
+      repo = insert(:repository)
+      workflow = insert(:workflow, repository: repo)
 
       insert(:workflow_run,
         head_sha: sha,
@@ -42,7 +49,8 @@ defmodule Dashy.Charts.WorkflowRunsTest do
         started_at: ~U[2021-05-16 09:00:00Z],
         completed_at: ~U[2021-05-16 10:00:00Z],
         status: "completed",
-        conclusion: "success"
+        conclusion: "success",
+        workflow: workflow
       )
 
       insert(:workflow_run,
@@ -51,10 +59,11 @@ defmodule Dashy.Charts.WorkflowRunsTest do
         started_at: ~U[2021-05-16 09:00:00Z],
         completed_at: ~U[2021-05-16 11:00:00Z],
         status: "completed",
-        conclusion: "failure"
+        conclusion: "failure",
+        workflow: workflow
       )
 
-      assert [%Dashy.Charts.Run{} = fetched_run] = Dashy.Charts.WorkflowRuns.runs()
+      assert [%Dashy.Charts.Run{} = fetched_run] = Dashy.Charts.WorkflowRuns.runs(repo)
 
       assert fetched_run.status == "error"
     end
